a162c13ef60e0a75646bf27b3443028c333601d3
Peinthor Rene <peinthor@gmail.com>
2013-11-13 21:59:15 +0100

    first working version

--- qmtag.py
+++ qmtag.py
@@ -3,12 +3,29 @@ __author__ = 'rp'
 import sys
 import os
 import argparse
+import shutil
+from mutagen.easyid3 import EasyID3
 
-defaultformat = "{trknr}-{artist}-{album}.{ext}"
+#defaultformat = "{tracknumber}-{artist}-{album}-{title}.{ext}"
+defaultformat = "{artist}/{album}/{tracknumber:02d}-{artist}-{title}.{ext}"
 supportedextensions = ['mp3', 'ogg']
 
 
+def id3todict(id3):
+    def get(d, k):
+        return "" if len(d[k]) == 0 else d[k][0]
+
+    data = dict()
+    data['artist'] = get(id3, 'artist')
+    data['album'] = get(id3, 'album')
+    data['title'] = get(id3, 'title')
+    data['date'] = int(get(id3, 'date'))
+    data['tracknumber'] = int(get(id3, 'tracknumber'))
+    return data
+
+
 def renamefiles(args):
+    drystr = "[dry]" if args.dry else ""
     if not args.format:
         args.format = defaultformat
 
@@ -18,14 +35,27 @@ def renamefiles(args):
         filelist = [x for x in os.listdir(args.in_dir)
                     if os.path.splitext(x)[1][1:] in supportedextensions]
         for mfile in filelist:
-            print(mfile)
-    print(args)
+            origpath = os.path.join(args.in_dir, mfile)
+            d = id3todict(EasyID3(origpath))
+            d['ext'] = os.path.splitext(x)[1][1:]
+            newpath = os.path.join(args.out_dir, args.format.format(**d))
+            if not 'ext' in args.format:
+                newpath += '.' + d['ext']
+            try:
+                os.makedirs(os.path.dirname(newpath))
+            except OSError as err:
+                if err.errno != 17:
+                    raise err
+            if not args.dry:
+                shutil.move(origpath, newpath)
+            print("{dry}moved: {path}".format(dry=drystr, path=newpath))
 
 
 def main():
     parser = argparse.ArgumentParser(description="Music file renamer based on tags")
     parser.add_argument('-f', '--format', help="formatting string to use")
     parser.add_argument('-r', '--recursive', action="store_true", help="look recursive in in_dir")
+    parser.add_argument('-d', '--dry', action="store_true", help="don't actually copy anything, just show")
     parser.add_argument('in_dir', help="search dir for music files")
     parser.add_argument('out_dir', help="output base directory")
 
